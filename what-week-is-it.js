906f542cb9a40c2488a5fc80baa73b8f41664d20
hcwf <h.frederich@protonmail.com>
2022-04-21 22:35:33 +0200

    Update what-week-is-it.js
    Added a new method to determine the current week and removed all deprecated methods.

--- what-week-is-it.js
+++ what-week-is-it.js
@@ -13,9 +13,6 @@
  * All variables to set up the current date.
  */
  let date = new Date();
- let day = date.getDate();
- let month = date.getMonth() + 1;
- let year = date.getYear();
 
 /**
  * All the variables needed for the widget.
@@ -47,81 +44,19 @@ wTextWeek.shadowRadius= 1;
 
 /**
  * Gets the current week of the year.
- * @param day
- * @param month
- * @param year
+ * @param date
  * @returns The current calendar week, starting Monday.
  */
-function getWeek(day, month, year) {
-    const NORMAL_YEAR = new Array(0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-    const LEAP_YEAR = new Array(0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
-    
-    let yearToUse;
-    
-    let currentDay = 0;
-    let week = 0;
-    let firstDay = getFirst();
+function getWeek(date) {
+    let firstOfYear;
+    let week;
 
-    if(firstDay > 4) {
-        week--;
-    }
+    date = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
+    date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
+    firstOfYear = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
+    week = Math.ceil((((date - firstOfYear) / 86400000) + 1) / 7);
 
-    if(isLeapYear(year)) {
-        yearToUse = LEAP_YEAR;
-    } else {
-        yearToUse = NORMAL_YEAR;
-    }
-   
-    for(let i = 0; i <= month; i++) {
-        currentDay += yearToUse[i];
-    }
-    currentDay += day;
-
-    week = Math.floor(currentDay / 7);
     return week;
-}
- 
-/**
- * Checks if the current year is a leap year
- * or not and returns it as a boolean.
- * @param year The year to check if it's a leap year for.
- * @returns Boolean if it's a leap year or not.
- */
-function isLeapYear(year) {
-    if(year % 400 == 0) {
-        return true;
-    }
-   
-    if(year % 100 == 0) {
-        return false;
-    }
-   
-    if(year % 4 == 0) {
-        return true;
-    }
-   
-    return false;
-}
- 
-/**
- * Gets the first day of the year as a
- * weekday.
- * @param year The year to get the first day for.
- * @returns First day of the year. 1 == Monday, 2 == Tuesday, etc..
- */
-function getFirst(year) { 
-    const FIRST_OF_2020 = 4;
-    let first;
-
-    let x = year - 2020;    
-    let y = Math.floor(x / 4); //Needed for leap years, as the following year starts 2 days later.
-    first = FIRST_OF_2020 + x + y;
-   
-    if(first > 7) {
-        first = first - 7;
-    }
-   
-    return first;
 }
 
 Script.setWidget(widget);
