7342d75712fa3a36d0eec249c28c4da240c7a555
hcwf <h.frederich@protonmail.com>
2022-04-26 20:59:25 +0200

    Update what-week-is-it.js
    Rewrote my old function(s) to not rely on "stolen" code.
    Added more commentary.

--- what-week-is-it.js
+++ what-week-is-it.js
@@ -6,16 +6,26 @@
  * What Week Is It?!
  * A JavaScript widget for Scriptable on iOS.
  * Author: hcwf (https://github.com/hcwf)
- * Version: 1.1.3 22.04.2022
+ * Version: 1.2.3 26.04.2022
+ * 
+ * NOTES:
+ * # I use "(current)" a lot in this script because it is mainly used
+ * to get the week of the currenty date. If you so please you can modify
+ * the script to calculate the week for any date. Just pass the appropriate
+ * parameters to the getWeek()-function.
  */
 
 /**
- * All variables to set up the current date.
+ * All variables to set up the (current) date.
  */
 let date = new Date();
+let dayofMonth = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getYear();
+let dayOfWeek = date.getDay() + 1;
 
 /**
- * All the variables needed for the widget.
+ * All the variables needed for the widget display.
  */
 const BG_GRADIENT = new LinearGradient();
 const GRADIENT_COLORS = [new Color("#7075ed"), new Color("#b4a1b")];
@@ -30,7 +40,7 @@ const TITLE_SIZE = new Size(120, 40);
 const WEEK_SIZE = new Size(120, 78);
 const SPACER_SIZE = new Size(10, 78);
 const NUMBER_SIZE = new Size(110, 78);
-const BORDER_WIDTH = 0;
+const BORDER_WIDTH = 0; //Only used for visually checking the stack layout when designing.
 
 let widget = new ListWidget();
 
@@ -64,7 +74,7 @@ numberStack.topAlignContent();
 numberStack.setPadding(0, 7, 5, 7);
 
 let wTextTitle = titleStack.addText("Calendar Week");
-let wTextWeek = numberStack.addText("" + getWeek(date));
+let wTextWeek = numberStack.addText("" + getWeek(day, dayOfMonth, year, dayOfWeek));
 
 BG_GRADIENT.colors = GRADIENT_COLORS;
 BG_GRADIENT.locations = [0, 1];
@@ -86,21 +96,88 @@ wTextWeek.shadowOffset = new Point(0, 3);
 let titleSpacer = titleStack.addSpacer(10);
 
 /**
- * Gets the current week of the year.
- * @param date The date object to work with.
- * @returns The current calendar week, starting Monday.
+ * Calculates the (current) week of the year using the algorithm 
+ * from Wikipedia. (https://en.wikipedia.org/wiki/ISO_week_date#Calculating_the_week_number_from_an_ordinal_date)
+ * @param day The day of the month. 1-31
+ * @param month The month of the year. 1-12
+ * @param year The (current) year.
+ * @param dayOfWeek The (current) day of the week. Monday = 1, Tuesday = 2, etc.
+ * @returns The (current) calendar week, starting Monday.
  */
-function getWeek(date) {
-    let firstOfYear;
-    let week;
+ function getWeek(dayOfMonth, month, year, dayOfWeek) {
+    /**
+     * Both constants start with 0 because the integers representing the 
+     * months are later added to the dayOfYear variable. To not 
+     * f up the number when in January you have to start with 0, otherwise 
+     * you'd end up with 31 + dayOfMonth.
+     * 
+     * And yes, I could have left the integer for the days of December out
+     * of the array. I just like it that way. :-) It's more "complete".
+     */
+    const NORMAL_YEAR = new Array(0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+    const LEAP_YEAR = new Array(0, 31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
 
-    //Algorithm by RobG https://stackoverflow.com/questions/6117814/get-week-of-year-in-javascript-like-in-php/6117889#6117889
-    date = new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate()));
-    date.setUTCDate(date.getUTCDate() + 4 - (date.getUTCDay() || 7));
-    firstOfYear = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
-    week = Math.ceil((((date - firstOfYear) / 86400000) + 1) / 7);
+    let yearToUse;
+
+    let dayOfYear = 0;
+    let week = 0;
+
+    let leapYear = isLeapYear(year);
+
+    if(leapYear) {
+        yearToUse = LEAP_YEAR;
+    } else {
+        yearToUse = NORMAL_YEAR;
+    }
+
+    for(let i = 0; i <= month; i++) {
+        dayOfYear += yearToUse[i];
+    }
+    dayOfYear += dayOfMonth;
+
+    week = Math.floor(((dayOfYear - dayOfWeek) + 10) / 7);
+
+    if(week == 0) {
+        if(leapYear){
+            week = 53;
+        } else {
+            week = 52;
+        }
+        return week;
+    }
+
+    if(week == 53) {
+        if(leapYear) {
+            
+        } else {
+            week = 1;
+        }
+        return week;
+    }
 
     return week;
+}
+
+/**
+ * Checks if the current year is a leap year
+ * or not and returns it as a boolean.
+ * @param year The year to check.
+ * @returns Boolean is true if it's a leap year, false if not.
+ */
+function isLeapYear(year) {
+    if(year % 400 == 0) {
+        return true;
+    }
+
+    if(year % 100 == 0) {
+        return false;
+    }
+
+    if(year % 4 == 0) {
+        return true;
+    }
+
+    return false;
 }
 
 Script.setWidget(widget);
